4151c2b24a7edd882a275a92fb1c1918b2551f88
Ankit Kumar Patel <patelankit1216@gmail.com>
2026-07-13 10:24:21 +0530

    updated

--- lib/site.ts
+++ lib/site.ts
@@ -1,3 +1,3 @@
 export const SITE_URL = "https://www.rishavsingh.design"; // TODO: replace with the production domain
 export const SITE_NAME = "Rishav Singh";
-export const CONTACT_EMAIL = "";
+export const CONTACT_EMAIL = "Rishav.desigm07@gmail.com";
